c11e4a542eee09010b9887ae2401048c84d17a52
Rakeshsheelam <rakeshsheelam98@outlook.com>
2024-12-18 17:04:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,20 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-southeast-2"
 }
 
 resource "aws_instance" "amzn_linux" {
-  ami           = "ami-05c13eab67c5d8861"
+  ami           = "ami-0d6560f3176dc9ec0"
   instance_type = "t2.micro"
-  key_name      = "linux"
-  vpc_security_group_ids = ["sg-04cb5f715d70d6f35"]
+  key_name      = "sydney"
   tags = {
     Name = "amz-linux"
   }
 }
 
 resource "aws_instance" "ubuntu_linux" {
-  ami           = "ami-0fc5d935ebf8bc3bc"
+  ami           = "ami-003f5a76758516d1e"
   instance_type = "t2.micro"
-  key_name      = "ubuntu-key"
-  vpc_security_group_ids = ["sg-04cb5f715d70d6f35"]
+  key_name      = "sydney"
   tags = {
     Name = "ubuntu22"
   }
